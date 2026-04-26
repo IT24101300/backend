5b1ee0b1a0d08f3caf40053fe365fd6aa9345f5e
JananiKavindya <kavindyajanani123@gmail.com>
2026-04-26 18:50:03 +0530

    new

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || '*'
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
